2f016b0e52b01aeaf20ced5a773d7b0348eba876
Ian Alderman <ialderman@hotmail.co.uk>
2020-09-21 19:56:23 +0100

    Updated exoeriments config

--- experiments/webApps/configureExperiments.sh
+++ experiments/webApps/configureExperiments.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Take the replace string
-read -p "Enter the Traffic Manager address (e.g. engasd.trafficmanager.net DO NOT INCLUDE the http://): " replace
+read -p "Enter the unique name that was generated for these experiments (e.g. http://adjalj.trafficmanager.net would be adjalj): " replace
 
 search="replaceme"
 
 if [[ $replace != "" ]]; then
 #sed -i "s/$search/$replace/" $filename
-find . -type f -name "/home/vscode/workspace/chaostoolkit-azure-demo/experiments/webApps/Experiment*" -exec sed -i "s/$search/$replace/g" {} \;
+find . -type f -wholename "/home/vscode/workspace/chaostoolkit-azure-demo/experiments/webApps/Experiment*" -exec sed -i "s/$search/$replace/g" {} \;
 fi
